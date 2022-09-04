615b27ad191808bca72d2066c8e3c7e1d8d5128f
dcbasham <dauriebasham@gmail.com>
2022-09-03 20:28:58 -0500

    codeform

--- src/scenes/Mainscene.js
+++ src/scenes/Mainscene.js
@@ -22,5 +22,13 @@ export default class MainScene extends Phaser.Scene {
 
     // CREATE SOCKET
     this.socket = io();
+
+    // Launch Waiting room.
+
+    scene.scene.launch('WaitingRoom', { socket: scene.socket });
+    //we do this so that it appears at the start of our game. and see below.
   }
+  update() {}
 }
+/* We launch the waiting room from here too so that we can pass it our socket. Instead of starting it on the game instance, so that we don't create a new one on waiting room. this would tell the server we have another player instead of connect the same one.
+NOTE: we will have to initialize it in waiting room with phaser's init() method. */
